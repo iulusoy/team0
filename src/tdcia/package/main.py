22a790da4c38ab3309013238ed7aaf1786d6a45d
iulusoy <inga.ulusoy@uni-heidelberg.de>
2021-03-19 14:58:43 +0100

    fixed folder structure

--- src/tdcia/package/main.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import statistical as sl
-
-if __name__ == "__main__":
-    test = np.array((1, 1, 1.2, 1.3, 3.5, 2.0))
-    dist = sl.euclidean_distance([0,2,4],[1,3,5],test)
-    print('Found distance {}'.format(dist))
